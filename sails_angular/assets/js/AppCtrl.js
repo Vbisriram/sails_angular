f1304b549585e8e0ab6d3232df35c32008f27890
Sriram B <sriramb@visualbi.com>
2015-02-04 20:22:19 +0530

    Added routes files

--- sails_angular/assets/js/AppCtrl.js
+++ sails_angular/assets/js/AppCtrl.js
@@ -1,23 +1,77 @@
-angular.module('myApp', [])
-  .controller('mainCtrl', function($scope, $http, $log) {
-    $scope.name = "Dinesh";
-    $scope.user = {
-      name: "default_name",
-      number: "12",
-      email: "default@mm.com"
-    };
-    $scope.submit = function() {
-      console.log('User clicked submit with ', $scope.user);
-      $http.post("http://localhost:1330/employee/create/", {
-          name: $scope.user.name,
-          empnum: $scope.user.number,
-          email: $scope.user.email
-        })
-        .success(function(data) {
-          $scope.employees = data;
-          $log.info($scope.employees);
+ var myApp = angular.module('myApp', ['ngRoute']);
 
-        });
-    }
+ myApp.controller('mainCtrl', function($scope, $http, $log, $location, $window) {
+ 	$scope.name = "Dinesh";
+ 	$scope.user = {
+ 		name: "default_name",
+ 		number: "12",
+ 		email: "default@mm.com"
+ 	};
+ 	$scope.returned = "";
 
-  });
+ });
+
+
+ myApp.config(['$routeProvider',
+ 	function($routeProvider) {
+ 		$routeProvider.
+ 		when('/Addemployee', {
+ 			//console.log("inside add new order");
+ 			templateUrl: 'templates/add-emp.html',
+ 			controller: 'AddOrderController'
+ 		}).
+ 		when('/viewemployee', {
+ 			templateUrl: 'templates/show-emp.html',
+ 			controller: 'ShowOrderController'
+ 		}).
+ 		when('/success', {
+ 				templateUrl: 'templates/success.html',
+ 				controller: 'SuccessController'
+ 			})
+ 			// 	.otherwise({
+ 			// 		redirectTo: '/'
+ 			// 	});
+ 	}
+ ]);
+
+ myApp.controller('AddOrderController', function($scope, $http, $log, $window) {
+ 	console.log("hello");
+ 	$scope.message = 'This is Add new order screen';
+ 	$scope.submit = function() {
+ 		$http.post("http://localhost:1330/employee/create", {
+ 				name: $scope.user.name,
+ 				empnum: $scope.user.number,
+ 				email: $scope.user.email
+ 			})
+ 			.success(function(data) {
+ 				$scope.employees = data;
+ 				$log.info($scope.employees);
+ 				$scope.returned = data;
+ 			});
+
+ 		//	console.log('User clicked submit with after the xcadad ', $scope.employees);
+ 		// 	$location.path('localhost:1330/success');
+ 		$window.location.href = '/success';
+
+ 	}
+ });
+
+
+ myApp.controller('ShowOrderController', function($scope) {
+ 	console.log("hello showing");
+ 	$scope.message = 'This is show order screen';
+
+ });
+
+ myApp.controller('SuccessController', function($scope) {
+ 	console.log("Success page showing");
+ 	$scope.message = 'This is success screen';
+
+ });
+ //
+ //
+ //  myapp.controller('ShowOrdersController', function($scope) {
+ //
+ //    $scope.message = 'This is Show orders screen';
+ //
+ //  });
